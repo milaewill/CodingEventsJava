0185d9be0655626f1064e056d413e224aa14a086
milaewill <milaewilliams@gmail.com>
2024-02-05 16:03:09 -0600

    exercises

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -0,0 +1,46 @@
+package org.launchcode.codingevents.controllers;
+
+
+import org.launchcode.codingevents.data.EventCategoryRepository;
+import org.launchcode.codingevents.models.EventCategory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/eventCategories")
+public class EventCategoryController {
+
+    private final EventCategoryRepository eventCategoryRepository;
+
+    public EventCategoryController(EventCategoryRepository eventCategoryRepository) {
+        this.eventCategoryRepository = eventCategoryRepository;
+    }
+
+    @GetMapping("/index")
+    public String displayAllCategories(Model model) {
+        model.addAttribute("title", "All Categories");
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "eventCategories/index";
+    }
+
+    @GetMapping("/create")
+    public String renderCreateEventCategoryForm(Model model) {
+        model.addAttribute("title", "Create Category");
+        model.addAttribute("eventCategory", new EventCategory());
+        return "eventCategories/create";
+    }
+
+    @PostMapping("/create")
+    public String processCreateEventCategoryForm(EventCategory eventCategory, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Category");
+            return "eventCategories/create";
+        }
+
+        return "redirect:/eventCategories/index";
+    }
+}
